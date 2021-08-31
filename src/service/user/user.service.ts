70c64df471532c7c06d03b7ac800b97191b339f8
Avadheshxprsweb <Avadhesh@xprsweb.com>
2021-08-31 23:20:04 +0530

    Issue fix.

--- src/service/user/user.service.ts
+++ src/service/user/user.service.ts
@@ -59,45 +59,46 @@ export class UserService {
   }
 
   public async create(req, userMstEntity: UserMstEntity): Promise<UserMstEntity> {
-    if(userMstEntity.email && userMstEntity.email.length > 0){
-      let emails = await this.userMstRepository.find({where : { email : userMstEntity.email}});
+    if (userMstEntity.email && userMstEntity.email.length > 0) {
+      let emails = await this.userMstRepository.find({ where: { email: userMstEntity.email } });
 
-      if(emails.length > 0){
+      if (emails.length > 0) {
         throw new BusinessException(Constants.FAILURE_CODE, "Email aleady exists");
       }
-      
+
     }
 
-    if(userMstEntity.userName && userMstEntity.userName.length > 0){
-      let userName = await this.userMstRepository.find({where : { userName : userMstEntity.userName}});
+    if (userMstEntity.userName && userMstEntity.userName.length > 0) {
+      let userName = await this.userMstRepository.find({ where: { userName: userMstEntity.userName } });
 
-      if(userName.length > 0){
+      if (userName.length > 0) {
         throw new BusinessException(Constants.FAILURE_CODE, "Username aleady exists");
       }
-      
+
     }
 
-    if(userMstEntity.contactNumber && userMstEntity.contactNumber.length > 0){
-      let contactNumber = await this.userMstRepository.find({where : { contactNumber : userMstEntity.contactNumber}});
+    if (userMstEntity.contactNumber && userMstEntity.contactNumber.length > 0) {
+      let contactNumber = await this.userMstRepository.find({ where: { contactNumber: userMstEntity.contactNumber } });
 
-      if(contactNumber.length > 0){
+      if (contactNumber.length > 0) {
         throw new BusinessException(Constants.FAILURE_CODE, "Contact no aleady exists");
       }
-      
+
     }
 
-    if(userMstEntity.password){
+    if (userMstEntity.password) {
       userMstEntity.password = this.passwordEncryptionService.encrypt(userMstEntity.password);
-    }else{
+    } else {
       userMstEntity.password = this.getUniqueCode();
       userMstEntity.password = this.passwordEncryptionService.encrypt(userMstEntity.password);
     }
 
-    let usr =  await this.userMstRepository.save(userMstEntity);
-    if(usr.email){
+    let usr = await this.userMstRepository.save(userMstEntity);
+    if (usr.email) {
       let mail = [];
       mail.push(usr.email);
-      await this.restCallService.sendMail(req, mail, "New Password", "Password-: "+this.passwordEncryptionService.decrypt(usr.password));
+      await this.restCallService.sendMail(req, mail, "TESC Logistics - A Revolution in 3PL", this.welcomeUserMailHtml.replace("[user]", usr.firstName));
+      await this.restCallService.sendMail(req, mail, "TESC Logistics - A Revolution in 3PL Password", "Password-: " + this.passwordEncryptionService.decrypt(usr.password));
     }
     return usr;
   }
@@ -222,90 +223,163 @@ export class UserService {
 
   async getRoleClaimMappingId(roleId: number): Promise<any> {
 
-      let role = await this.roleRepository.findOne(roleId);
-      let claims = await this.claimMasterRepository.find({ where: { roleId: role.id } })
-      let entityList = await this.entityMasterRepository.findByIds(claims.map(val => val.entityId));
-      let entity;
-      let json = {};
+    let role = await this.roleRepository.findOne(roleId);
+    let claims = await this.claimMasterRepository.find({ where: { roleId: role.id } })
+    let entityList = await this.entityMasterRepository.findByIds(claims.map(val => val.entityId));
+    let entity;
+    let json = { };
 
-      
-      for (let index = 0; index < claims.length; index++) {
-        const element = claims[index];
-        entity = entityList.find(val => val.id == element.entityId);
 
-        if (!json[entity.groupName]) {
-          json[entity.groupName] = []
-        }
+    for (let index = 0; index < claims.length; index++) {
+      const element = claims[index];
+      entity = entityList.find(val => val.id == element.entityId);
 
-        element.entityMaster = entityList.find(val => val.id == claims[index].entityId);
-        json[entity.groupName].push(element)
-
+      if (!json[entity.groupName]) {
+        json[entity.groupName] = []
       }
-      let list = [];
 
-      let keys = Object.keys(json);
-      for (let index = 0; index < keys.length; index++) {
-        const element = keys[index];
-        list.push(JSON.parse("{\""+element+"\" : "+JSON.stringify(json[element])+"}"));
-      }
-      
-      
-      return list;
+      element.entityMaster = entityList.find(val => val.id == claims[index].entityId);
+      json[entity.groupName].push(element)
+
+    }
+    let list = [];
+
+    let keys = Object.keys(json);
+    for (let index = 0; index < keys.length; index++) {
+      const element = keys[index];
+      list.push(JSON.parse("{\"" + element + "\" : " + JSON.stringify(json[element]) + "}"));
+    }
+
+
+    return list;
 
   }
 
   async getRoleClaimForUpdate(roleId: number): Promise<any> {
-        
-      let role = await this.roleRepository.findOne(roleId);
-      let claims = await this.claimMasterRepository.find({ where: { roleId: role.id } })
-      let entityList = await this.entityMasterRepository.find();
-      let entity;
-      let json = {};
-      let claim : ClaimMasterEntity;
 
-      for (let e = 0; e < entityList.length; e++) {
-         entity = entityList[e];
-         if (!json[entity.groupName]) {
-          json[entity.groupName] = []
-        }       
-        claim = claims.find(val => val.entityId == entity.id);  
-        
-        if(claim){
-          claim.entityMaster = entity;
-          json[entity.groupName].push(claim);
-        }else{
-          claim = new ClaimMasterEntity();
-          claim.entityId = entity.id;
-          claim.entityMaster = entity;
-          claim.roleId = roleId;
-          claim.isCreate = false;
-          claim.isDelete = false;
-          claim.isExport = false;
-          claim.isImport = false;
-          claim.isPurge = false;
-          claim.isRead = false;
-          claim.isUpdate = false;
-          json[entity.groupName].push(claim);
-        }
-        
+    let role = await this.roleRepository.findOne(roleId);
+    let claims = await this.claimMasterRepository.find({ where: { roleId: role.id } })
+    let entityList = await this.entityMasterRepository.find();
+    let entity;
+    let json = { };
+    let claim: ClaimMasterEntity;
+
+    for (let e = 0; e < entityList.length; e++) {
+      entity = entityList[e];
+      if (!json[entity.groupName]) {
+        json[entity.groupName] = []
+      }
+      claim = claims.find(val => val.entityId == entity.id);
+
+      if (claim) {
+        claim.entityMaster = entity;
+        json[entity.groupName].push(claim);
+      } else {
+        claim = new ClaimMasterEntity();
+        claim.entityId = entity.id;
+        claim.entityMaster = entity;
+        claim.roleId = roleId;
+        claim.isCreate = false;
+        claim.isDelete = false;
+        claim.isExport = false;
+        claim.isImport = false;
+        claim.isPurge = false;
+        claim.isRead = false;
+        claim.isUpdate = false;
+        json[entity.groupName].push(claim);
       }
 
-      let list = [];
+    }
 
-      let keys = Object.keys(json);
-      for (let index = 0; index < keys.length; index++) {
-        const element = keys[index];
-        list.push(JSON.parse("{\""+element+"\" : "+JSON.stringify(json[element])+"}"));
-      }
-      
-      
-      return list;
+    let list = [];
+
+    let keys = Object.keys(json);
+    for (let index = 0; index < keys.length; index++) {
+      const element = keys[index];
+      list.push(JSON.parse("{\"" + element + "\" : " + JSON.stringify(json[element]) + "}"));
+    }
+
+
+    return list;
 
   }
 
-  async updateRoleClaims(claims : ClaimMasterEntity[]){
+  async updateRoleClaims(claims: ClaimMasterEntity[]) {
     this.claimMasterRepository.save(claims);
 
   }
+
+
+  welcomeUserMailHtml = "<style>\n" +
+    ".font{\n" +
+    "font-family:'Segoe UI','Apple SD Gothic Neo','Lucida Grande','Lucida Sans Unicode','sans-serif';\n" +
+    "}\n" +
+    "</style>\n" +
+    "<div bgcolor=\"#FFFFFF\" marginwidth=\"0\" marginheight=\"0\">\n" +
+    "    <table id=\"m_2515312712125314003Table_01\" width=\"600\" align=\"center\" border=\"0\" cellpadding=\"0\" cellspacing=\"0\">\n" +
+    "      <tbody><tr>\n" +
+    "        <td style=\"background:rgb(0, 0, 0);text-align:center;padding:20px 0\" class=\"font\">\n" +
+    "          <a href=\"https://tesc.in/\" target=\"_blank\" title=\"TESC Logistics Redefined\"><img src=\"https://i1.wp.com/tesc.in/wp-content/uploads/2021/03/cropped-Tesc-Logo-Website-05-1.png?w=500&ssl=1\" height=\"150\" alt=\"TESC Logistics Redefined\" class=\"tesc\"></a>\n" +
+    "        </td>\n" +
+    "      </tr>\n" +
+    "\t  <tr>\n" +
+    "        <td style=\"    background: rgb(247, 247, 247);text-align: left;padding: 20px;font-size: 16px;font-weight: 400;\" class=\"font\">\n" +
+    "          Dear [user],\n" +
+    "        </td>\n" +
+    "      </tr>\n" +
+    "\t  <tr>\n" +
+    "        <td style=\"background:rgb(247, 247, 247); text-align: center; padding: 20px;  font-size: 20px;\"class=\"font\">\n" +
+    "          Thank you for Register at  <br><a style=\"text-decoration: none; color: #000; font-size: 30px; font-weight: 800;\" title=\"TESC Logistics Redefined\" href=\"https://tesc.in/\" target=\"_blank\">TESC Logistics Redefined</a>\n" +
+    "\t\t  <br>We will reach you very soon as Possible !!!\n" +
+    "        </td>\n" +
+    "      </tr>\n" +
+    "      <tr>\n" +
+    "        <td style=\"padding:20px 0;background:#efefef\">\n" +
+    "          <table width=\"100%\" cellpadding=\"0\" cellspacing=\"0\">\n" +
+    "            <tbody><tr>\n" +
+    "\t\t\t   \n" +
+    "              <td style=\"font-size:16px;color:#373435;text-align:justify;margin:20px 20px;background:#ffffff;letter-spacing:.68px;padding:15px;border-radius:5px;display:block;font-weight:600\" class=\"font\">\n" +
+    "              TESC Logistics - A Revolution in 3PL\n" +
+    "              <br>\n" +
+    "\t\t\t  <br>\n" +
+    "\t\t\t  Tech Enabled Supply Chain(Tesc), brings to you convenience and efficiency while moving your goods. Our focus is simplifying logistics via tech that helps our customers to move their freight with ease. \n" +
+    "              <br>\n" +
+    "\t\t\t  <br>\n" +
+    "\t\t\t  \n" +
+    "</td>\n" +
+    "            </tr>\n" +
+    "          </tbody></table>\n" +
+    "        </td>\n" +
+    "      </tr>\n" +
+    "      <tr>\n" +
+    "        <td style=\"background-color:#000000;padding:25px 0 25px 30px\">\n" +
+    "          <table style=\"width:100%\" cellpadding=\"0\" cellspacing=\"0\">\n" +
+    "            <tbody><tr>\n" +
+    "              <td>\n" +
+    "                <table style=\"width:100%\" cellpadding=\"0\" cellspacing=\"0\">\n" +
+    "                  <tbody><tr>\n" +
+    "                    <td style=\"font-size:20px;font-weight:700;color:#ffffff\" class=\"font\">Thanks,</td>\n" +
+    "                  </tr>\n" +
+    "                  <tr>\n" +
+    "                    <td style=\"font-size:14px;color:#ffffff\" class=\"font\">TESC Logistics Redefined Team</td>\n" +
+    "                  </tr>\n" +
+    "                  <tr>\n" +
+    "                    <td><a href=\"mailto:enquiry@tesc.in\" title=\"enquiry@tesc.in\" class=\"font\" style=\"font-size:14px;color:#ffffff;text-decoration:none\" target=\"_blank\">enquiry@tesc.in</a>\n" +
+    "                  </td>\n" +
+    "                </tr>\n" +
+    "              </tbody></table>\n" +
+    "            </td>\n" +
+    "          </tr>\n" +
+    "\n" +
+    "</tbody></table>\n" +
+    "</td>\n" +
+    "</tr>\n" +
+    "<tr>\n" +
+    "        <td style=\"background:#fff; text-align: center; padding: 5px 0;  font-size: 12px;\" class=\"font\">\n" +
+    "          Design and Develop by <a style=\"text-decoration: none; font-weight: 600; color: #000;;\" title=\"TESC Logistics Redefined\" href=\"https://www.vihaainfotech.com/\" title=\"Vihaa Infotech\" target=\"_blank\">Vihaa Infotech</a>\n" +
+    "        </td>\n" +
+    "      </tr>\n" +
+    "</tbody></table>\n" +
+    "</div>"
 
 }
